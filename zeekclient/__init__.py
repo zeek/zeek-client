e9c4948c4f53daa21897fa99f7ed6937616dacf3
Christian Kreibich <christian@corelight.com>
2022-09-24 16:34:38 -0700

    Remove Broker from the package's __init__.py and restructure imports
    The package no longer requires Broker. This also restructures the imports so not
    all contents get imported into the zeekclient namespace. It's cleaner to keep
    the events and types modules separate.

--- zeekclient/__init__.py
+++ zeekclient/__init__.py
@@ -6,23 +6,17 @@ for request/response events.
 """
 import sys
 
-try:
-    import broker
-except ImportError:
-    print('error: zeek-client requires the Python Broker bindings.\n'
-          'Make sure your Zeek build includes them. To add installed\n'
-          'Broker bindings to Python search path manually, add the\n'
-          'output of "zeek-config --python_dir" to PYTHONPATH.')
-    sys.exit(1)
+from . import brokertypes
+from . import events
+from . import types
 
 from .config import *
 from .cli import *
 from .consts import *
 from .controller import *
-from .events import *
 from .logs import *
-from .types import *
 from .utils import *
 
 __version__ = "1.1.0-5"
-__all__ = ['cli', 'config', 'consts', 'controller', 'events', 'logging', 'types', 'utils']
+__all__ = ['brokertypes', 'cli', 'config', 'consts', 'controller',
+           'events', 'logging', 'types', 'utils']
