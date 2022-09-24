378bad6ee0c76069e1fb7716bd361a1012ba02e4
Christian Kreibich <christian@corelight.com>
2022-09-24 16:23:07 -0700

    Fix typos in docstrings

--- zeekclient/__init__.py
+++ zeekclient/__init__.py
@@ -1,7 +1,7 @@
 """This package defines a Python interface for interacting with a Zeek cluster
 controller. It provides a Controller class for sending and retrieving events, a
 Zeek event abstraction on top of Broker, and various types that mirror those
-defined in Zeek's policy/management/frameworks/management/types.zeek, needing
+defined in Zeek's policy/management/frameworks/management/types.zeek, needed
 for request/response events.
 """
 import sys
